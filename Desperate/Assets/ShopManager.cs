7d33293937552c66b59bbc0210de77d30647b0cc
Jack Maxwell <Jack.maxwell0360@gmail.com>
2020-04-20 14:50:21 -0400

    bRUH

--- Desperate/Assets/ShopManager.cs
+++ Desperate/Assets/ShopManager.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class ShopManager : MonoBehaviour
 {
     public Text moneyT;
-  
+
+    void Awake()
+    {
+        
+    }
+
     public void buyAmmo()
     {
         if(PlayerManager.getMoney() > 150)
